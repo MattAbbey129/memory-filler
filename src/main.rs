400cd80cd7dc2afe98a59d79522aed0c4bedd4e9
MattAbbey129 <Borrowing_Lushness974@SimpleLogin.com>
2025-09-16 23:22:18 -0400

    docs: add extra comments for context

--- src/main.rs
+++ src/main.rs
@@ -23,6 +23,10 @@ fn main() -> Result<()> {
 
     let mut stdout: Stdout = stdout();
 
+    /*
+        Initialize terminal cursor position so we
+        know where to print the buffer statistics.
+    */
     stdout
         .queue(cursor::MoveToColumn(0))
         .with_context(|| "Unable to move cursor to first column")?;
@@ -41,8 +45,9 @@ fn main() -> Result<()> {
         let buffer_length: usize = buffer.len();
 
         /*
-            Allocate more memory in the vector when we've written the
-            amount of bytes in CLUSTER_SIZE since the last allocation.
+            Allocate more memory to the buffer and print buffer statistics
+            when we have written the amount of bytes in CLUSTER_SIZE since
+            the last allocation or if there is nothing in the buffer yet.
         */
         if buffer_length % CLUSTER_SIZE == 0 {
             stdout
@@ -52,6 +57,12 @@ fn main() -> Result<()> {
                 .queue(terminal::Clear(terminal::ClearType::FromCursorDown))
                 .with_context(|| "Unable to clear terminal from cursor down")?;
 
+            /*
+                We manually allocate an exact additional amount of memory to the buffer
+                instead of letting Rust speculatively allocate additional memory for us.
+                Since we know what we need, we will take care of it instead, since Rust
+                may not pick up the pattern of allocation for the buffer.
+            */
             buffer
                 .try_reserve_exact(CLUSTER_SIZE)
                 .with_context(|| format!("Unable to allocate {CLUSTER_SIZE} bytes in memory! Did we ran out of memory?"))?;
