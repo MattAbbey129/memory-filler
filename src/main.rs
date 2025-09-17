67ccca6060415936bc1c8905f2e11399d84d43f2
MattAbbey129 <Borrowing_Lushness974@SimpleLogin.com>
2025-09-16 22:42:15 -0400

    refactor: display buffer information on one line instead of new lines

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 // SPDX-License-Identifier: MIT
 // Copyright © 2025 Matt Abbey
 
+use std::io::{stdout, Stdout, Write};
+
 use anyhow::{Context, Result};
+use crossterm::{cursor, style::Print, terminal, QueueableCommand};
 
 fn main() -> Result<()> {
 
@@ -16,6 +19,18 @@ fn main() -> Result<()> {
         ONE_MEGABYTE
     };
 
+    let mut stdout: Stdout = stdout();
+
+    stdout
+        .queue(cursor::MoveToColumn(0))
+        .with_context(|| "Unable to move cursor to first column")?;
+    stdout
+        .queue(cursor::SavePosition)
+        .with_context(|| "Unable to save cursor position")?;
+    stdout
+        .flush()
+        .with_context(|| "Unable to execute terminal instructions for initializing the terminal cursor")?;
+
     loop {
         /*
             Avoid needlessly computing 'buffer.len()' multiple
@@ -28,11 +43,23 @@ fn main() -> Result<()> {
             amount of bytes in CLUSTER_SIZE since the last allocation.
         */
         if buffer_length % CLUSTER_SIZE == 0 {
+            stdout
+                .queue(cursor::RestorePosition)
+                .with_context(|| "Unable to restore cursor position")?;
+            stdout
+                .queue(terminal::Clear(terminal::ClearType::FromCursorDown))
+                .with_context(|| "Unable to clear terminal from cursor down")?;
+
             buffer
                 .try_reserve_exact(CLUSTER_SIZE)
                 .with_context(|| format!("Unable to allocate {CLUSTER_SIZE} bytes in memory! Did we ran out of memory?"))?;
 
-            println!("Buffer: {} bytes", buffer_length);
+            stdout
+                .queue(Print(format!("Buffer: {} bytes", buffer_length)))
+                .with_context(|| "Unable to display buffer information to stdout")?;
+            stdout
+                .flush()
+                .with_context(|| "Unable to execute terminal instructions for displaying buffer information")?;
         }
 
         buffer.push(0);
