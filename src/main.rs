24d81c4d999e4f7ed466c571bd85ead000f047cd
MattAbbey129 <Borrowing_Lushness974@SimpleLogin.com>
2025-09-16 22:12:55 -0400

    feat: display how much memory the buffer uses on each 1 MB write cycle

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
 // SPDX-License-Identifier: MIT
 // Copyright © 2025 Matt Abbey
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 
 fn main() -> Result<()> {
 
     let mut buffer: Vec<u8> = Vec::new();
 
+    /*
+        I avoid setting magic numbers by making
+        the intent of this specific number clear.
+    */
+    const CLUSTER_SIZE: usize = {
+        const ONE_MEGABYTE: usize = 1_048_576;
+        ONE_MEGABYTE
+    };
+
     loop {
+        if buffer.len() % CLUSTER_SIZE == 0 {
+            println!("Buffer: {} bytes", buffer.len());
+        }
+
         buffer.push(0);
     }
 
